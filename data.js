8ef7c34466f0c590e86d9032fd9e2cdd080351dd
Gabriel Cursi <gaabrielcursi@gmail.com>
2021-03-05 07:14:06 -0300

    finalizando integracao nunjucks e colocando dados dinamicos

---
+++ data.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    id: "construindo-um-app-com-mapas-usando-react-native-maps-e-mapbox",
+    img: "https://blog.rocketseat.com.br/content/images/2020/10/teste.jpg",
+    title: "Construindo App com Mapa usando React Native Maps e MapBox",
+    author: "por Thiago Marinho",
+    reading_time: "8 min de leitura"
+  },
+  {
+    id: "dark-mode-com-css-mudando-a-aparencia-do-blog-de-maneira-simples-e-rapida",
+    img: "https://blog.rocketseat.com.br/content/images/2020/10/dark-mode-com-css-mudando-a-aparencia-do-blog-de-maneira-simples-e-rapida.jpg",
+    title: "Dark Mode com CSS — mudando a aparência do Blog de maneira simples e rápida",
+    author: "por Thiago Marinho",
+    reading_time: "3 min de leitura"
+  },
+  {
+    id: "upload-de-imagens-no-front-end-com-react-js-e-context-api-3",
+    img: "https://blog.rocketseat.com.br/content/images/2020/10/blog-rocketseat-upload-de-imagens-no-front-end-com-react-js-e-context-api.png",
+    title: "Upload de imagens no Front End com ReactJS e Context API",
+    author: "por Thiago Marinho",
+    reading_time: "17 min de leitura"
+  },
+]
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
+const articles = require("./data");
 
 const server = express();
 
@@ -12,24 +13,44 @@ nunjucks.configure("views", {
 });
 
 server.get("/", function (req, res) {
-  return res.render("courses");
+  return res.render("courses", { articles });
 });
 
 server.get("/about", function (req, res) {
-  return res.render("about");
+  const about = {
+    logo: "https://rocketseat.com.br/icons/icon-512x512.png",
+    name: "Rocketseat",
+    description:
+      "Trazendo o melhor e mais completo conteúdo gratuíto de programação para quem quer começar a desenvolver sites.",
+    languages: [
+      { name: "HTML 5" },
+      { name: "ReactJS" },
+      { name: "React Native" },
+      { name: "Java Script" },
+    ],
+    links: [
+      {
+        url: "https://github.com/Rocketseat",
+        name: "Github",
+      },
+      {
+        url: "https://www.instagram.com/rocketseat_oficial/",
+        name: "Instagram",
+      },
+      {
+        url: "http://www.facebook.com/rocketseat/",
+        name: "Facebook",
+      },
+    ],
+  };
+
+  return res.render("about", { about });
 });
 
-
-
-
-
-
-
-server.use(function(req, res) {
+server.use(function (req, res) {
   res.status(404).render("not-found");
 });
 
-server.listen(3000, function() {
-  console.log('Server is running')
-})
-
+server.listen(3000, function () {
+  console.log("Server is running");
+});
